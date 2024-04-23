217b8a1c2789f71be54b3ff06a9f17148e5124a9
dayzorgno <164095538+dayzorgno@users.noreply.github.com>
2024-04-23 10:19:52 -0300

    Update models.py

--- ZZ/HOTELapp/models.py
+++ ZZ/HOTELapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Buffet(models.Model):
+    horario = models.CharField(max_length=100)
+    menu = models.TextField()
+
+    def __str__(self):
+        return self.horario
