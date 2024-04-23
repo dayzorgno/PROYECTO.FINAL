de5bc8423a5fa739ad7203be146e37ef6363dfe1
dayzorgno <164095538+dayzorgno@users.noreply.github.com>
2024-04-23 10:25:16 -0300

    Delete ZZ directory

--- ZZ/HOTELapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('bienvenidos/', views.bienvenidos, name='bienvenidos'),
-    path('reserva-buffet/', views.reserva_buffet, name='reserva_buffet'),
-]
